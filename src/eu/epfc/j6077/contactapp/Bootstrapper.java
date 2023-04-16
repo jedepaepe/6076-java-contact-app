ce2ab8775ba9bf44abcd1876d8b35b4932202d01
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-04-16 08:58:18 +0200

    3 create table

--- src/eu/epfc/j6077/contactapp/Bootstrapper.java
+++ src/eu/epfc/j6077/contactapp/Bootstrapper.java
@@ -1,9 +1,14 @@
 package eu.epfc.j6077.contactapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Bootstrapper {
     public static void main(String[] args) {
+        initializeDb();
         Scanner input = new Scanner(System.in);
         String choice;
         do {
@@ -15,5 +20,22 @@ public class Bootstrapper {
             System.out.println("(Q) quitter l'application");
             choice = input.nextLine();
         } while (! choice.equals("Q"));
+    }
+
+    private static void initializeDb() {
+        String sql = "create table if not exists CONTACT (" +
+                "  ID integer primary key auto_increment," +
+                "  FIRSTNAME varchar(32) not null," +
+                "  LASTNAME varchar(32) not null," +
+                "  EMAIL varchar(128)," +
+                "  PHONE varchar(20)" +
+                ")";
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:./contact");
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
