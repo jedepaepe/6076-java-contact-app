42e257d4313813dee4690f251a89c279c079c67f
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-04-16 08:38:20 +0200

    2 menu loop

--- src/eu/epfc/j6077/contactapp/Bootstrapper.java
+++ src/eu/epfc/j6077/contactapp/Bootstrapper.java
@@ -1,7 +1,19 @@
 package eu.epfc.j6077.contactapp;
 
+import java.util.Scanner;
+
 public class Bootstrapper {
     public static void main(String[] args) {
-        System.out.println("debug - application started");
+        Scanner input = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.println("Choisissez dans les options suivantes");
+            System.out.println("(1) lister les contacts");
+            System.out.println("(2) ajouter un contact");
+            System.out.println("(3) modifier un contact");
+            System.out.println("(4) supprimer un contact");
+            System.out.println("(Q) quitter l'application");
+            choice = input.nextLine();
+        } while (! choice.equals("Q"));
     }
 }
